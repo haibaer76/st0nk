521c0b16c127d70be9507c10ab8a18635b0c153e
Torsten Fehre <feson@ganesha.(none)>
2009-10-08 11:08:04 +0200

    Added Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,10 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
+  map.login '/login', :controller => 'account', :action => 'login'
+  map.logout '/logout', :controller => 'account', :action => 'logout'
+  map.signup '/signup', :controller => 'account', :action => 'signup'
+
   map.connect 'docs/:docname', :controller => 'documents', :action => 'find_by_name', 
     :docname => /([a-zA-Z0-9\-_]*)/
   map.connect 'docs/:docname.:branch', :controller => 'documents', :action => 'find_by_name',
